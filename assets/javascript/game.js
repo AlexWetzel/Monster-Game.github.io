f5339efa796bdab60b35b344faad0afb424746ad
AlexWetzel <wetzela@live.com>
2017-08-15 18:25:37 -0400

    Very bare-bones game 'working'.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,7 @@ var game = {
 		console.log(game.rathalos);
 
 		monster.attr("atk", stats.attack);
+		monster.attr("base", stats.attack);
 		monster.attr("cAtk", stats.counterAtk);
 		monster.attr("hp", stats.healthPoints);
 
@@ -31,6 +32,7 @@ var rath = $("#rathalos");
 var tig = $("#tigrex");
 var zino = $("#zinogre");
 var gore = $("#gore-magala");
+var combo = 1;
 
 game.assignStats(game.rathalos, rath);
 game.assignStats(game.tigrex, tig);
@@ -58,14 +60,39 @@ $(document).on("click", "#enemies>div", function() {
 
 });
 
+
+//When the attack button is hit
 $(document).on("click", "#attack", function() {
-	var monster = $("#players>div")
+	console.log(combo);
+//The player attacks the defender. the defender takes damage equal to the player's attack
 
-	monster.attr("hp", monster.attr("hp") - 10);
+	var player = $("#players>div");
+	var defender = $("#defenders>div");	
+	// var monster = $("#players>div")
 
-	game.updateStats("#players>div>p", monster);
+	defender.attr("hp", defender.attr("hp") - player.attr("atk"));
 
+//The player's attack is boosted after a successful attack
+	player.attr("atk", parseInt(player.attr("atk")) + parseInt(player.attr("base")));
+
+	game.updateStats("#players>div>p", player);
+
+//The defender's stats are updated
+	game.updateStats("#defenders>div>p", defender);
+
+//If the defender has no hp, they are removed from the game
+	if (defender.attr("hp") <= 0) {
+		defender.remove();
+		game.defenderSelect = false;
+	}
+	else {
+//The defender counterattacks, dealing damage to the player equal to the defender's counter stat
+	player.attr("hp", player.attr("hp") - defender.attr("cAtk"));
+	game.updateStats("#players>div>p", player);
+	}
 	// $("#players>div>p").text("Attack: " + monster.attr("atk") + " Counter: " + monster.attr("cAtk") + " Health: " + monster.attr("hp"));
+
+
 });
 
 // $("#attack")on("click", function() {
