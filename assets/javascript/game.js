dceb634ec9699cb76f5692db3aa191eec396cfc6
AlexWetzel <wetzela@live.com>
2017-08-17 15:36:41 -0400

    added images that appear when selecting a character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 var game = {
-	rathalos: {attack: 10, counterAtk: 10, healthPoints: 100},
-	tigrex: {attack: 11, counterAtk: 11, healthPoints: 101},
-	zinogre: {attack: 10, counterAtk: 10, healthPoints: 100},
-	gore_magala: {attack: 10, counterAtk: 10, healthPoints: 100},
+	rathalos: {name: "Rathalos", attack: 10, counterAtk: 27, healthPoints: 180},
+	tigrex: {name: "Tigrex", attack: 20, counterAtk: 25, healthPoints: 120},
+	zinogre: {name: "Zinogre", attack: 16, counterAtk: 30, healthPoints: 130},
+	gore_magala: {name: "Gore Magala", attack: 9, counterAtk: 21, healthPoints: 200},
 
 	playerSelect: false,
 	defenderSelect: false,
@@ -11,58 +11,106 @@ var game = {
 
 		console.log(game.rathalos);
 
+		monster.attr("name", stats.name);
 		monster.attr("atk", stats.attack);
 		monster.attr("base", stats.attack);
 		monster.attr("cAtk", stats.counterAtk);
 		monster.attr("hp", stats.healthPoints);
 
-		console.log("Attack: " + monster.attr("atk") + " Counter: " + monster.attr("cAtk") + " Health: " + monster.attr("hp"));
-		monster.append("<p>Attack: " + monster.attr("atk") + " Counter: " + monster.attr("cAtk") + " Health: " + monster.attr("hp") + "</p>");
+		// console.log("Attack: " + monster.attr("atk") + " Counter: " + monster.attr("cAtk") + " Health: " + monster.attr("hp"));
+		monster.append("<p>Attack: " + monster.attr("atk") + "<br> Counter: " + monster.attr("cAtk") + "<br> Health: " + monster.attr("hp") + "</p>");
 	},
 
 	updateStats: function(stats, monster) {
 		$(stats).text("Attack: " + monster.attr("atk") + " Counter: " + monster.attr("cAtk") + " Health: " + monster.attr("hp"));
+	},
+
+	// hoover: function(select) {
+	// 	select.not(".render").hover(function(){
+	// 		$(this).find(".render").fadeIn("fast");
+	// 	},
+	// 	function(){
+	// 		$(this).find(".render").fadeOut("fast");
+	// 	});
+
+	showRender: function(selection){
+		if(game.playerSelect === false){
+			$(selection).mouseenter(function(){
+				$(selection).find(".render").stop(true).fadeIn("fast");		
+			}).mouseleave(function(){
+					if (game.playerSelect !== true) {
+						$(selection).find(".render").hide();
+					}
+			});
+		}
 	}
 
+	// hideRender: function(selection){
 
-
+	// 	$(selection).mouseleave(function(){
+	// 		console.log(game.playerSelect);
+	// 		$(selection).find(".render").hide();
+	// 	});
+	// }
+	
 }
 
 var rath = $("#rathalos");
 var tig = $("#tigrex");
 var zino = $("#zinogre");
 var gore = $("#gore-magala");
-var combo = 1;
+var i = 3;
 
 game.assignStats(game.rathalos, rath);
 game.assignStats(game.tigrex, tig);
 game.assignStats(game.zinogre, zino);
 game.assignStats(game.gore_magala, gore);
 
+
+
+$(".render").hide();
+
+
+
+game.showRender(rath);
+game.showRender(tig);
+game.showRender(zino);
+game.showRender(gore);
+
 $(document).on("click", "#players>div", function() {
 
-	if (game.playerSelect == false) {
+	if (game.playerSelect === false) {
+
 		var selectChar = $(".character").not(this).remove();
 
 		selectChar.appendTo("#enemies");
 
 		game.playerSelect = true;
-	}
-
+		$("#message").text("Choose your target");
+		$(this).find(".render").stop(true);
+		console.log("test");
+	};
 });
 
+
+
+
+	
 $(document).on("click", "#enemies>div", function() {	
 
-	if (game.defenderSelect == false){
+	if (game.defenderSelect === false){
 
 		var selectEnemy = $(this).remove();
 
 		selectEnemy.appendTo("#defenders");
 
 		game.defenderSelect = true;
+		$("#message").text("Hit the FIGHT button to battle!")
+		$(this).stop();
 	}
-
 });
+
+
 
 
 //When the attack button is hit
@@ -70,11 +118,11 @@ $(document).on("click", "#attack", function() {
 	//If a defender is selected, the player is allowed to attack
 	if(game.defenderSelect){		
 	//The player attacks the defender. the defender takes damage equal to the player's attack
-
 		var player = $("#players>div");
 		var defender = $("#defenders>div");	
 		
 		defender.attr("hp", defender.attr("hp") - player.attr("atk"));
+		$("#message").text(player.attr("name") + " attacked " + defender.attr("name") + ", dealing " + player.attr("atk") + " damage!");
 
 	//The player's attack is boosted after a successful attack
 		player.attr("atk", parseInt(player.attr("atk")) + parseInt(player.attr("base")));
@@ -88,20 +136,29 @@ $(document).on("click", "#attack", function() {
 		if (defender.attr("hp") <= 0) {
 			defender.remove();
 			game.defenderSelect = false;
-		}
-		else {
+			i--;
+			console.log(i);
+			if (i === 0) {
+				$("#message").append("<br>" + defender.attr("name") + " is defeated! You win! <br> (refresh the page to start over)");
+
+			}
+			else {
+				$("#message").append("<br>" + defender.attr("name") + " is defeated! Choose your next target.");
+			}
+		} 
+		
+		else{
 	//The defender counterattacks, dealing damage to the player equal to the defender's counter stat
 		player.attr("hp", player.attr("hp") - defender.attr("cAtk"));
 		game.updateStats("#players>div>p", player);
+		$("#message").append("<br>" + defender.attr("name") + " retaliates, dealing " + defender.attr("cAtk") + " damage.");
+			
+			if (player.attr("hp") <= 0) {
+				game.defenderSelect = false;
+				$("#message").append("<br> You have been defeated! <br> (refresh the page to start over)");
+			}
 		}
 		// $("#players>div>p").text("Attack: " + monster.attr("atk") + " Counter: " + monster.attr("cAtk") + " Health: " + monster.attr("hp"));
 	}
 
 });
-
-// $("#attack")on("click", function() {
-
-// });
-
-
-
